82ceb39e3c1c782d5ec93b6d48f408c7486dc34b
Md Najmul Haque <najmulhaque008@gmail.com>
2022-11-25 11:53:51 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from 'cors'
+
+const app = express();
+
+//middleware 
+app.use(cors());
+app.use(express.json()) // to parse json file, we use it
+
+//root api
+app.get('/', (req, res) => {
+    res.status(200).send({
+        success: true,
+        message: "inventory management server is running"
+    })
+})
+
+export default app;
