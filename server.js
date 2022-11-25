7ff3e04290a140d936b8fec7f6264df5a78e3396
Md Najmul Haque <najmulhaque008@gmail.com>
2022-11-25 12:32:03 +0600

    mongoDB connection established

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ import app from './app.js'
 
 //Database connection 
 mongoose.connect(process.env.DATABASE).then(() => {
-    console.log('Database connection is successful')
+    console.log('Database connection is successful'.blue.bold)
 })
 
 // server
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 5000
 
 app.listen(port, () => {
-    console.log(`Server is running on port, ${port}`.yellow.bold);
+    console.log(`Server is running on port, ${port}`.red.bold);
 })
